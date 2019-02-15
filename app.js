fbc06368b549db576f4ab735088468ae7710b773
Zinan <zinan.ma.1014@gmail.com>
2019-02-15 15:45:33 -0500

    modified signup.html and app.js

--- app.js
+++ app.js
@@ -12,12 +12,63 @@ app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static(__dirname));
 
 
-//mongoose.connect("mongodb://localhost:27017/todolistDB",{useNewUrlParser: true});
+mongoose.connect("mongodb://localhost:27017/SUCAFE",{useNewUrlParser: true});
 
+const userSchema = {
+  fname: String,
+  lname: String,
+  netid:String,
+  password:String,
+  occupation: String,
+  email: String
+}
+
+const User = mongoose.model("user",userSchema);
+
+const user1 = new User({
+  fname:"Zinan",
+  lname:"Ma",
+  netid:"123",
+  password:"String",
+  occupation: "OC",
+  email: "1@e.com"
+});
+
+user1.save();
 
 app.get("/",function(req,res){
 res.sendFile( __dirname + "/home.html");//__dirname:current file location
 });
+
+
+app.post("/signin.html",function(req,res){
+  console.log("statusCode for Signin.html");
+  console.log(res.statusCode);
+   console.log(req.body.netID);
+   console.log(req.body.password);
+});
+
+app.post("/signup.html",function(req,res){
+     // console.log(req.body);
+
+  const firstName = req.body.fName;
+  const lastName = req.body.lName;
+  const netID =req.body.netid;
+  const passWord =req.body.pWord;
+  const email =req.body.email;
+
+  // if the id does not exist
+  //create an doc in database
+
+  console.log("statusCode for Signup.html");
+  console.log(res.statusCode);
+
+  if (res.statusCode===200){
+    res.sendFile(__dirname+"/success.html");
+  }
+
+});
+
 
 
 
