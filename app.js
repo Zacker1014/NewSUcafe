1353bbd63e3071edd7951fc948df1f6327df5e4e
Zinan <zinan.ma.1014@gmail.com>
2019-02-15 16:37:27 -0500

    complete the functionality of  signup page

--- app.js
+++ app.js
@@ -7,6 +7,8 @@ const mongoose = require("mongoose");
 const app = express();
 
 app.set('view engine', 'ejs');
+app.use(express.static('/'))
+
 
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static(__dirname));
@@ -34,7 +36,7 @@ const user1 = new User({
   email: "1@e.com"
 });
 
-user1.save();
+//user1.save();
 
 app.get("/",function(req,res){
 res.sendFile( __dirname + "/home.html");//__dirname:current file location
@@ -49,23 +51,52 @@ app.post("/signin.html",function(req,res){
 });
 
 app.post("/signup.html",function(req,res){
-     // console.log(req.body);
 
+ console.log(req.body);
+ //for each new user
   const firstName = req.body.fName;
   const lastName = req.body.lName;
   const netID =req.body.netid;
   const passWord =req.body.pWord;
-  const email =req.body.email;
+  const ocupaTion = req.body.selectpicker
+  const uemail =req.body.email;
+
 
   // if the id does not exist
   //create an doc in database
 
+    User.findOne({
+      netid: netID
+    }, function(err, foundList) {
+      if (!err) {
+        if (!foundList) {
+          //create a new list
+          const user = new User({
+            fname: firstName,
+            lname: lastName,
+            netid: netID,
+            password: passWord,
+            occupation: ocupaTion,
+            email: uemail
+          });
+          user.save();
+          if (res.statusCode===200){
+            res.sendFile(__dirname+"/success.html");
+          }
+        } else {
+        //  console.log("id:""+netID);
+          //netid already exist
+          res.sendFile(__dirname+"/failure.html");
+        }
+      }
+    });
+
+
+
   console.log("statusCode for Signup.html");
   console.log(res.statusCode);
 
-  if (res.statusCode===200){
-    res.sendFile(__dirname+"/success.html");
-  }
+
 
 });
 
