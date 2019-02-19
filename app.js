142881d835fbad9371060ad5b3d36b874faaca0d
Zinan <zinan.ma.1014@gmail.com>
2019-02-19 11:32:54 -0500

    Manager and employee datatable fixed

--- app.js
+++ app.js
@@ -333,6 +333,6 @@ Order.find({}, function(err, foundItems) {
 });
 
 
-app.listen(3060, function() {
-  console.log("Server started on port 3060");
+app.listen(3000, function() {
+  console.log("Server started on port 3000");
 });
